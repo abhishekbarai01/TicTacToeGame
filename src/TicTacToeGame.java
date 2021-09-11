50d55f6f9e59d718165473845b823408bba2296c
Abhishek <abhishekb042@gmail.com>
2021-09-11 09:54:56 +0530

    added chooseletter method in Tic Tac Toe Game

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -3,18 +3,39 @@ import java.util.Scanner;
 public class TicTacToeGame {
     public static void main(String[] args) {
         Board(); // call Board method
+        chooseLetter(); // call chooseLetter method
 
     }
 
     static void Board() {  // create board method
         System.out.println("Welcome to TicTacToe Game");
-        String[] Board = new String[10]; // Array length 10
-        for (int i = 0; i <= Board.length; i++) {
-            Board[i] = " ";
-
+        String[] board = new String[10]; // Array length 10
+        for (int i = 0; i < board.length; i++) {
+            board[i] = " ";
         }
     }
+
+    static void chooseLetter() {   // create chooseletter method
+        System.out.println("Enter a character :");
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+
+        if (ch == 'O') {                    // If - else
+            String computer = "X";
+            String Player = "O";
+            System.out.println("Player choose " + Player);
+            System.out.println("Computer choose " + computer);
+        } else if (ch == 'X') {
+            String computer = "O";
+            String Player = "X";
+            System.out.println("Player choose " + Player);
+            System.out.println("Computer choose " + computer);
         }
+        else{
+            System.out.println("Invalid Input");
+        }
+    }
+}
 
 
 
